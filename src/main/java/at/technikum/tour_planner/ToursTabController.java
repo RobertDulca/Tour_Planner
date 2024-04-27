a951b4465d0e88a3c53c60b726dfeadeda6a4608
Umair Saqub <if22b029@technikum-wien.at>
2024-04-28 01:01:05 +0200

    Menu, Search-Bar, Tours Tab added

---
+++ src/main/java/at/technikum/tour_planner/ToursTabController.java
@@ -0,0 +1,29 @@
+package at.technikum.tour_planner;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+
+public class ToursTabController {
+
+    @FXML
+    public Button addTourButton;
+    @FXML
+    public Button removeTourButton;
+    @FXML
+    public Button editTourButton;
+    @FXML
+    private ListView <String> toursList;
+
+    @FXML
+    public void onAddTour() {
+    }
+
+    @FXML
+    public void onRemoveTour() {
+    }
+
+    @FXML
+    public void onEditTour() {
+    }
+}
