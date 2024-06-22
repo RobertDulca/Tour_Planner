4cf4cd9c1dd4b07440e35aec3594a1cc026ffb4b
Umair Saqub <if22b029@technikum-wien.at>
2024-06-22 23:14:44 +0200

    import rm

--- src/main/java/at/technikum/tour_planner/TourPlannerApplication.java
+++ src/main/java/at/technikum/tour_planner/TourPlannerApplication.java
@@ -1,7 +1,6 @@
 package at.technikum.tour_planner;
 
 import at.technikum.tour_planner.event.Publisher;
-import at.technikum.tour_planner.view.*;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
